c3593a5ff96e985a09a0ac452d01588fa6a9e778
= <=>
2021-10-08 09:52:46 -0300

    Incluido persistencia Postegresql

--- src/main/java/dominio/Usuario.java
+++ src/main/java/dominio/Usuario.java
@@ -7,16 +7,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import javassist.SerialVersionUID;
 
 @Entity
 public class Usuario {	
-	
-	
+		
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String nome;
+	
+	@Temporal(TemporalType.DATE)
 	private Date dtNascimento;
 	private String curso;
 	private String periodo;	
